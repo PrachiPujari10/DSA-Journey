f26b379aecb6f8be626693a8765537571169d2c4
PrachiPujari10 <prachipujari338@gmail.com>
2026-03-03 12:38:13 +0530

    added Day21

---
+++ Day21/H-IndexII.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int low = 0, high = n - 1;
+        
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if(citations[mid] >= n - mid) {
+                high = mid - 1;   // try to find smaller index
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return n - low;
+    }
+}
