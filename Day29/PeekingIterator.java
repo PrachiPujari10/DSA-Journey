6ec46db3db14874fbfb9d29bb5947421171ebd02
PrachiPujari10 <prachipujari338@gmail.com>
2026-03-11 19:25:49 +0530

    Added day 29

---
+++ Day29/PeekingIterator.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class PeekingIterator implements Iterator<Integer> {
+
+    private Iterator<Integer> iterator;
+    private Integer nextElement;
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+        this.iterator = iterator;
+        if (iterator.hasNext()) {
+            nextElement = iterator.next();
+        }
+    }
+
+    public Integer peek() {
+        return nextElement;
+    }
+
+    @Override
+    public Integer next() {
+        Integer result = nextElement;
+
+        if (iterator.hasNext()) {
+            nextElement = iterator.next();
+        } else {
+            nextElement = null;
+        }
+
+        return result;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return nextElement != null;
+    }
+}
